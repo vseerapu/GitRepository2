ebdd16eb1031dd7def1a773fed917681c1fb0d9d
vseerapu <vseerapu@gmail.com>
2022-10-12 17:29:52 -0600

    First commit....

---
+++ src/com/myproject/TestCase1.java
@@ -0,0 +1,11 @@
+package com.myproject;
+
+public class TestCase1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Practicing git push pull and merge");
+
+	}
+
+}
