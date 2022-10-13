b4201585bfd9d760581f13eb681cadfd4b5e9a0a
vseerapu <vseerapu@gmail.com>
2022-10-12 18:58:13 -0600

    Adding TestCase2...

---
+++ src/com/myproject/TestCase2.java
@@ -0,0 +1,12 @@
+package com.myproject;
+
+public class TestCase2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("This is my second test case");
+
+	}
+
+}
